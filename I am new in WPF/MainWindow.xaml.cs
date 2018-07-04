d2b414b8f082fb9dcdbd461224008305dff07c21
Виктор Горбань <VictorGorban1999@yandex.ru>
2018-07-04 04:52:27 +0300

    Design 1.1. Also designed expr box. Now it's RichTextBox, we can format it with text.

--- I am new in WPF/MainWindow.xaml.cs
+++ I am new in WPF/MainWindow.xaml.cs
@@ -11,11 +11,49 @@ namespace I_am_new_in_WPF
     /// </summary>
     public partial class MainWindow
     {
-        
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void NumberButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var button = sender.Cast<Button>();
+            var text = button.Content as string;
+            ExpressionBlock.InsertIntoCaretPosition(text, button.Foreground.ToString());
+        }
+
+        private void BraceButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (true) // no complicated handlers. It just add the brace to expression.
+            {
+                var button = sender.Cast<Button>();
+                var text = button.Content as string;
+                ExpressionBlock.InsertIntoCaretPosition(text, button.Foreground.ToString());
+            }
+        }
+
+        private void OperationButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (true) // handler: if last symvol in the expr is an op, then replace it, else just add to the expression.
+            {
+                var button = sender.Cast<Button>();
+                var text = button.Content as string;
+                ExpressionBlock.InsertIntoCaretPosition(text, button.Foreground.ToString());
+            }
+        }
+
+        private void ClearButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ExpressionBlock.Clear();
+        }
+
+        private void BackspaceButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ExpressionBlock.PerformBackspace();
+        }
+
 
     }
 }
---
+++ I am new in WPF/RichTextBoxExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace I_am_new_in_WPF
+{
+    public static class RichTextBoxExtensions
+    {
+        public static void InsertIntoCaretPosition(this RichTextBox box, string text, string color)
+        {
+            BrushConverter converter = new BrushConverter();
+            TextRange range = new TextRange(box.CaretPosition, box.CaretPosition);
+            range.Text = text;
+            try
+            {
+                range.ApplyPropertyValue(TextElement.ForegroundProperty,
+                    converter.ConvertFromString(color));
+            }
+            catch (FormatException) {/*Nothing. If error, just set property to default (solid black brush)*/ }
+
+            box.RunNextPosition(LogicalDirection.Forward, text.Length);
+        }
+
+        public static void AppendText(this RichTextBox box, string text, string color)
+        {
+            BrushConverter converter = new BrushConverter();
+            TextRange range = new TextRange(box.Document.ContentEnd, box.Document.ContentEnd);
+            range.Text = text;
+            try
+            {
+                range.ApplyPropertyValue(TextElement.ForegroundProperty,
+                    converter.ConvertFromString(color));
+            }
+            catch (FormatException) {/*Nothing. If error, just set property to default (solid black brush)*/ }
+            
+            box.RunNextPosition(LogicalDirection.Forward, text.Length);
+        }
+
+        public static void Clear(this RichTextBox box)
+        {
+            box.Document.Blocks.Clear();
+        }
+
+        public static void PerformBackspace(this RichTextBox box)
+        {
+            var range = box.CaretPosition.GetPositionAtOffset(1,LogicalDirection.Backward);
+            range.DeleteTextInRun(1);
+            
+            box.RunNextPosition(LogicalDirection.Backward);
+        }
+
+        public static void RunNextPosition(this RichTextBox box, LogicalDirection direction = LogicalDirection.Forward, int count = 1)
+        {
+            box.CaretPosition = box.CaretPosition.GetPositionAtOffset(count, direction);
+        }
+
+
+    }
+}
