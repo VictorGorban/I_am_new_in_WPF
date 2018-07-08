72b92986bb3e802c4762b2d98d9ea639824b2033
Виктор Горбань <VictorGorban1999@yandex.ru>
2018-07-09 01:31:15 +0300

    Fixed the issue with blured elements on screen

--- I am new in WPF/MainWindow.xaml.cs
+++ I am new in WPF/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using Handy_Extensions;
+using WindowsInput;
 
 namespace I_am_new_in_WPF
 {
@@ -21,7 +23,21 @@ namespace I_am_new_in_WPF
         {
             var button = sender.Cast<Button>();
             var text = button.Content as string;
-            ExpressionBlock.InsertIntoCaretPosition(text, button.Foreground.ToString());
+            var key = text[0];
+            ExpressionBlock.Focus();
+            var focused = ExpressionBlock.IsFocused;
+            
+            //target.RaiseEvent(new KeyEventArgs(Keyboard.PrimaryDevice, PresentationSource.FromDependencyObject(this),0, (Key)key));
+            var inputSimulator = new InputSimulator();
+            
+            
+            inputSimulator.Mouse.MoveMouseBy(0,-60);
+            inputSimulator.Mouse.LeftButtonClick();
+            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)key);
+
+            focused = ExpressionBlock.IsFocused;
+            /*ExpressionBlock.InsertIntoCaretPosition(text, button.Foreground.ToString());
+            ExpressionBlock.Focus();*/
         }
 
         private void BraceButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -30,17 +46,21 @@ namespace I_am_new_in_WPF
             {
                 var button = sender.Cast<Button>();
                 var text = button.Content as string;
+                
                 ExpressionBlock.InsertIntoCaretPosition(text, button.Foreground.ToString());
             }
         }
 
-        private void OperationButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void OperationButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             if (true) // handler: if last symvol in the expr is an op, then replace it, else just add to the expression.
             {
                 var button = sender.Cast<Button>();
                 var text = button.Content as string;
+                
                 ExpressionBlock.InsertIntoCaretPosition(text, button.Foreground.ToString());
+                
+
             }
         }
 
