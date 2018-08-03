53e03238d84d7f97553f83ec400c37eb3595b96c
Виктор Горбань <VictorGorban1999@yandex.ru>
2018-08-03 20:35:23 +0300

    Fixed some extra code. And crapcode.

--- I am new in WPF/MainWindow.xaml.cs
+++ I am new in WPF/MainWindow.xaml.cs
@@ -19,28 +19,17 @@ namespace I_am_new_in_WPF
             InitializeComponent();
         }
 
-        private void NumberButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void NumberButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             var button = sender.Cast<Button>();
             var text = button.Content as string;
             var key = text[0];
-            ExpressionBlock.Focus();
-            var focused = ExpressionBlock.IsFocused;
-            
-            //target.RaiseEvent(new KeyEventArgs(Keyboard.PrimaryDevice, PresentationSource.FromDependencyObject(this),0, (Key)key));
-            var inputSimulator = new InputSimulator();
-            
-            
-            inputSimulator.Mouse.MoveMouseBy(0,-60);
-            inputSimulator.Mouse.LeftButtonClick();
-            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)key);
 
-            focused = ExpressionBlock.IsFocused;
-            /*ExpressionBlock.InsertIntoCaretPosition(text, button.Foreground.ToString());
-            ExpressionBlock.Focus();*/
+            ExpressionBlock.InsertIntoCaretPosition(text, button.Foreground.ToString());
+            ExpressionBlock.Focus();
         }
 
-        private void BraceButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void BraceButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             if (true) // no complicated handlers. It just add the brace to expression.
             {
@@ -59,17 +48,15 @@ namespace I_am_new_in_WPF
                 var text = button.Content as string;
                 
                 ExpressionBlock.InsertIntoCaretPosition(text, button.Foreground.ToString());
-                
-
             }
         }
 
-        private void ClearButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void ClearButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             ExpressionBlock.Clear();
         }
 
-        private void BackspaceButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void BackspaceButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             ExpressionBlock.PerformBackspace();
         }
