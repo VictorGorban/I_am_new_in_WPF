129531ef7c87c61cbd9599ad99a8f90baf35b29c
Виктор Горбань <VictorGorban1999@yandex.ru>
2018-08-05 08:51:06 +0300

    Started implementing mvvm

---
+++ I am new in WPF/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_am_new_in_WPF
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		#region fields
+		private string _buttonHistoryLabel;
+		#endregion
+		#region events
+		public event PropertyChangedEventHandler PropertyChanged;
+		#endregion
+
+		#region props
+		public string ButtonHistoryLabel { get => _buttonHistoryLabel; set => _buttonHistoryLabel = value; }
+		#endregion
+
+		public MainViewModel()
+		{
+			_buttonHistoryLabel = "HIST";
+		}
+	}
+}
